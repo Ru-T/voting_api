2ed399e05ee1bac9ae9dae5c3db31c1464db47bd
Ru-T <rwajnberg@gmail.com>
2015-09-23 12:53:11 -0400

    Create access_token on voter and require before making post or patch requests.

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,4 +2,12 @@ class Voter < ActiveRecord::Base
   validates :name, presence: true
   has_one :vote
 
+  before_create :generate_access_token
+
+  private def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922181910) do
+ActiveRecord::Schema.define(version: 20150923164809) do
 
   create_table "candidates", force: :cascade do |t|
     t.string   "name"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20150922181910) do
   create_table "voters", force: :cascade do |t|
     t.string   "name"
     t.string   "party"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "access_token"
   end
 
   create_table "votes", force: :cascade do |t|
